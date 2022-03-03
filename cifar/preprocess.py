a38c6343515138ac6a3d6691377497e4084869a5
LongJin <61048219+LongJin-lab@users.noreply.github.com>
2022-03-03 17:24:09 +0800

    Add files via upload

---
+++ cifar/preprocess.py
@@ -0,0 +1,115 @@
+import torch
+
+from torchvision import datasets, transforms
+
+
+# CIFAR-10,
+# mean, [0.4914, 0.4822, 0.4465]
+# std, [0.2470, 0.2435, 0.2616]
+# CIFAR-100,
+# mean, [0.5071, 0.4865, 0.4409]
+# std, [0.2673, 0.2564, 0.2762]
+def load_data(args):
+    if args.dataset_mode == "CIFAR10":
+        transform_train = transforms.Compose([
+            transforms.RandomCrop(32, padding=4),
+            transforms.RandomHorizontalFlip(),
+            transforms.ToTensor(),
+            transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+        ])
+
+        transform_test = transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+        ])
+        train_loader = torch.utils.data.DataLoader(
+            datasets.CIFAR10('data', train=True, download=True, transform=transform_train),
+            batch_size=args.batch_size,
+            shuffle=True,
+            num_workers=8
+        )
+
+        test_loader = torch.utils.data.DataLoader(
+            datasets.CIFAR10('data', train=False, transform=transform_test),
+            batch_size=args.batch_size,
+            shuffle=False,
+            num_workers=8
+        )
+    elif args.dataset_mode == "CIFAR100":
+        transform_train = transforms.Compose([
+            transforms.RandomCrop(32, padding=4),
+            transforms.RandomHorizontalFlip(),
+            transforms.ToTensor(),
+            transforms.Normalize((0.5071, 0.4865, 0.4409), (0.2673, 0.2564, 0.2762)),
+        ])
+
+        transform_test = transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Normalize((0.5071, 0.4865, 0.4409), (0.2673, 0.2564, 0.2762)),
+        ])
+        train_loader = torch.utils.data.DataLoader(
+            datasets.CIFAR100('data', train=True, download=True, transform=transform_train),
+            batch_size=args.batch_size,
+            shuffle=True,
+            num_workers=16
+        )
+
+        test_loader = torch.utils.data.DataLoader(
+            datasets.CIFAR100('data', train=False, transform=transform_test),
+            batch_size=args.batch_size,
+            shuffle=False,
+            num_workers=16
+        )
+    elif args.dataset_mode == "IMAGENET":
+        transform_train = transforms.Compose([transforms.RandomResizedCrop(224),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])]
+        )
+
+        transform_test = transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+        ])
+
+        train_dataset = datasets.ImageFolder(root='../../../../media3/datasets/imagenet/train', transform=transform_train)
+        train_loader = torch.utils.data.DataLoader(
+            train_dataset,
+            batch_size=args.batch_size,
+            shuffle=True,
+            drop_last=True,
+            num_workers=16
+        )
+        test_dataset = datasets.ImageFolder(root='../../../../media3/datasets/imagenet/val', transform=transform_test)
+        test_loader = torch.utils.data.DataLoader(
+            test_dataset,
+            batch_size=args.batch_size,
+            shuffle=False,
+            drop_last=True,
+            num_workers=16
+        )
+    elif args.dataset_mode == "MNIST":
+        transform_train = transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Normalize((0.1307,), (0.3081,)),
+        ])
+
+        transform_test = transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Normalize((0.1307,), (0.3081,)),
+        ])
+        train_loader = torch.utils.data.DataLoader(
+            datasets.CIFAR100('data', train=True, download=True, transform=transform_train),
+            batch_size=args.batch_size,
+            shuffle=True,
+            num_workers=2
+        )
+
+        test_loader = torch.utils.data.DataLoader(
+            datasets.CIFAR100('data', train=False, transform=transform_test),
+            batch_size=args.batch_size,
+            shuffle=True,
+            num_workers=2
+        )
+
+    return train_loader, test_loader
